7dc502763de62e316d94a3eb7194679556be870e
genych <evgenyshe+git@gmail.com>
2022-12-05 12:08:42 +0200

    migrate

---
+++ migrations/Version20221205100513.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221205100513 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE answer (
+          id INT AUTO_INCREMENT NOT NULL,
+          question_id INT NOT NULL,
+          fixed_numeric INT DEFAULT NULL,
+          free_text LONGTEXT DEFAULT NULL,
+          INDEX IDX_DADD4A251E27F6BF (question_id),
+          INDEX IDX_fixed (fixed_numeric),
+          PRIMARY KEY(id)
+        ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE question (
+          id INT AUTO_INCREMENT NOT NULL,
+          name VARCHAR(255) NOT NULL,
+          type VARCHAR(255) NOT NULL,
+          PRIMARY KEY(id)
+        ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE survey (
+          id INT AUTO_INCREMENT NOT NULL,
+          name VARCHAR(255) NOT NULL,
+          questions JSON NOT NULL,
+          PRIMARY KEY(id)
+        ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE
+          answer
+        ADD
+          CONSTRAINT FK_DADD4A251E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE answer DROP FOREIGN KEY FK_DADD4A251E27F6BF');
+        $this->addSql('DROP TABLE answer');
+        $this->addSql('DROP TABLE question');
+        $this->addSql('DROP TABLE survey');
+    }
+}
