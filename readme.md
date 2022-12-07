607f806132a2fe0866f44c0e908b2b87049ebc1c
genych <evgenyshe+git@gmail.com>
2022-12-07 15:51:00 +0200

    one more test, readme

--- readme.md
+++ readme.md
@@ -4,9 +4,17 @@ minimalistic but not bare bones php setup + survey demo app
 
 __how:__
 
-`docker-compose up -d` — spawn a bunch of containers
+`docker-compose up -d` — spawn a bunch of containers (wait for database to be pre-filled)
+
+`docker-compose exec php composer test` — run unit tests and static analyzer
 
 `https://localhost` — it's alive! (http is ok too)
+
+GET http://localhost/stats — answer statistics
+
+POST http://localhost/survey — submit survey
+
+GET http://localhost/survey/1 — get survey info
 
 
 __gotchas:__
@@ -16,7 +24,11 @@ also, it will expire one day. renew manually: https://letsencrypt.org/docs/certi
 
 __todo/changelog:__
 
-* logic
-* fixtures
-* tests
+* ~~logic~~
+* ~~fixtures~~
+* ~~tests~~
+* docs/openAPI
 * CI
+* more meaningfull and ordered tests
+* get rid of mixed remnants
+* profile
--- tests/Service/SurveyServiceTest.php
+++ tests/Service/SurveyServiceTest.php
@@ -2,9 +2,69 @@
 
 namespace App\Tests\Service;
 
+use App\Contract\QuestionType;
+use App\Entity\Answer;
+use App\Entity\Question;
+use App\Repository\AnswerRepository;
+use App\Repository\QuestionRepository;
+use App\Repository\SurveyRepository;
+use App\Service\SurveyService;
+use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
 
 class SurveyServiceTest extends TestCase
 {
+    private Question $question;
 
+    /** @var Answer[] */
+    private array $answers;
+
+    public function setUp(): void
+    {
+        // be careful, they don't have id. use reflection to fake them
+        $this->question = new Question('free', QuestionType::FREE_TEXT);
+        $this->answers = [
+            new Answer($this->question, null, 'some text'),
+            new Answer($this->question, null, 'another text comment'),
+        ];
+    }
+
+    /**
+     * not exhaustive and quite brittle. just example how to test service methods without DB.
+     */
+    public function testWordFrequency(): void
+    {
+        // stub everything DB-related
+        $em = $this->createStub(EntityManagerInterface::class);
+        $surveyRepo = $this->createStub(SurveyRepository::class);
+
+        $questionRepo = $this->createStub(QuestionRepository::class);
+        $questionRepo->method('findBy')->willReturn([$this->question]);
+
+        $answerRepo = $this->createStub(AnswerRepository::class);
+        $answerRepo->method('getText')->willReturnCallback($this->fakeText(...));
+        $answerRepo->method('count')->willReturnCallback(fn($_): int => count($this->answers));
+
+        // but service is real
+        $service = new SurveyService($surveyRepo, $questionRepo, $answerRepo, $em);
+
+        $stats = $service->freeTextStats();
+        $words = $stats->getTopWords();
+
+        // not good assertions because it doubles StatsTest
+        self::assertEquals(2, $stats->getAnswers());
+        self::assertArrayHasKey('text', $words);
+        self::assertEquals(2, $words['text']);
+    }
+
+    /**
+     * @param Question $question
+     * @return iterable<?string>
+     */
+    private function fakeText(Question $question): iterable
+    {
+        foreach ($this->answers as $answer) {
+            yield $answer->getFreeText();
+        }
+    }
 }
